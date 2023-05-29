395b9466a4cfeecd08ce719f570bda7a08c57d0e
kuuuuumiiiii <129586216+kuuuuumiiiii@users.noreply.github.com>
2023-05-29 08:24:54 +0000

    マイページ編集

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,13 +1,25 @@
 class Public::UsersController < ApplicationController
   def my_page
     @user = current_user
+    @posts = @user.posts
   end
 
-  def show
+  def profile
     @user = User.find(params[:id])
   end
 
   def edit
+    @user = User.find(params[:id])
+  end
 
+  def update
+    user = User.find(params[:id])
+    user.update(user_params)
+    redirect_to my_page_users_path
+  end
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :introduction, :image)
   end
 end
